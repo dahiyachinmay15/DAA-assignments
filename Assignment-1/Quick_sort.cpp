16ff563148cba22b05e332e3a2e9db05867c0d2d
Chinmay <dahiyachinmay15@gmail.com>
2026-01-19 16:53:14 +0530

    Add iterative quicksort implementation

--- Assignment-1/Quick_sort.cpp
+++ Assignment-1/Quick_sort.cpp
@@ -1,3 +1,4 @@
+//recursive
 #include<iostream>
 using namespace std;
 
@@ -32,4 +33,54 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+}
+// iterative
+#include<iostream>
+using namespace std;
+
+int partition(int arr[],int low,int high){
+    int pivot= arr[high];
+    int i= low-1;
+    for(int j=low; j<high; j++){
+        if(arr[j]<pivot){
+            i++;
+            int temp = arr[j];
+            arr[j]= arr[i];
+            arr[i]=temp;
+        }
+    }
+    int temp = arr[high];
+    arr[high]=arr[i+1];
+    arr[i+1]=temp;
+    return i+1;
+
+}
+
+void quicksort(int arr[], int n){
+    
+    int stack[n], top =-1;
+    stack[++top]=0;
+    stack[++top]=n-1;
+    while (top>=0){
+        int high =stack[top--];
+        int low =stack[top--];
+        int pi = partition(arr,low,high);
+        if(pi-1>low){
+            stack[++top]=low;
+            stack[++top]=pi-1;
+        }
+        if(pi+1<high){
+            stack[++top]=pi+1;
+            stack[++top]=high;
+        }
+    }
+}
+int main(){
+    int arr[]={4,6,1,32,12,23,16,31};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    quicksort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
 }
